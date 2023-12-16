beaa2adee8e85da81be515ca19b0ba06b3d29936
Oxamyt <dentez.danil@gmail.com>
2023-12-16 10:47:40 +0200

    make-functions-style

--- script.js
+++ script.js
@@ -1,19 +1,71 @@
 const myLibrary = [];
 
-function Book(title, author, status){
+function Book(title, author, pages, status){
     this.title = title;
     this.author = author;
+    this.pages = pages;
     this.status = status;
 }
 
-function addBookToLibrary(){
-    event.preventDefault();
-    const title =document.querySelector("#title")
-    const author =document.querySelector("#author")
-    const status =document.querySelector('input[name=status]:checked')
-    let newBook = new Book (title.value, author.value, status.value)
-    myLibrary.push(newBook);
+Book.prototype.readStatus = function(){
+    this.status = !this.status;
 }
 
-const button = document.querySelector('#add');
-button.addEventListener('click', addBookToLibrary)
+function readChange (index){
+    myLibrary[index].readStatus();
+    displayBook();
+}
+
+function addBookToLibrary(event){
+    event.preventDefault();
+    const title =document.querySelector("#title").value
+    const pages =document.querySelector("#pages").value
+    const author =document.querySelector("#author").value
+    const status =document.querySelector('#status').checked
+    let newBook = new Book (title, author, pages, status)
+    myLibrary.push(newBook);
+    displayBook();
+}
+
+function displayBook(){
+    const library =document.querySelector(".library");
+    library.innerHTML = '';
+    for (let i = 0; i < myLibrary.length; i++){
+        let book = myLibrary[i];
+        let card = document.createElement('div')
+        card.innerHTML = `<div class="card">
+        <h1 class="title">${book.title}</h1>
+        <h2 class="author"> By: ${book.author}</h2>
+        <h3 class="pages">Number of pages: ${book.pages}</h3>
+        <h4 class="status">${book.status ? 'Read' : 'Not read yet'}</h4>
+        <button class="remove" onclick='removeBook(${i})'>Remove Book</button>
+        <button class="toggle" onclick='readChange(${i})'>Change read status</button>
+      </div>`
+        library.appendChild(card)
+    }
+}
+
+
+function removeBook(index){
+    myLibrary.splice(index, 1);
+    displayBook();
+}
+
+const formDiv = document.querySelector('.form-cont');
+const bookForm = document.querySelector('.book-form');
+const addBtn = document.querySelector('.add-book')
+addBtn.addEventListener('click', function(){
+    if (formDiv.style.display = 'none'){
+        bookForm.style.display = 'flex';
+        formDiv.style.display = 'block'; 
+    }  
+    else if (bookForm.style.display = 'flex'){
+        bookForm.style.display = 'none';
+        formDiv.style.display = 'none'
+    }
+})
+
+const btnAdd = document.querySelector('.add');
+btnAdd.addEventListener('click', addBookToLibrary);
+
+

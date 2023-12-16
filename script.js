b13d88d516e463f513e5a4fd47e81a6b3959946c
Oxamyt <dentez.danil@gmail.com>
2023-12-16 12:40:29 +0200

    make-responsive-form

--- script.js
+++ script.js
@@ -1,71 +1,67 @@
 const myLibrary = [];
 
-function Book(title, author, pages, status){
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.status = status;
+function Book(title, author, pages, status) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.status = status;
 }
 
-Book.prototype.readStatus = function(){
-    this.status = !this.status;
+Book.prototype.readStatus = function () {
+  this.status = !this.status;
+};
+
+function readChange(index) {
+  myLibrary[index].readStatus();
+  displayBook();
 }
 
-function readChange (index){
-    myLibrary[index].readStatus();
-    displayBook();
+function addBookToLibrary(event) {
+  event.preventDefault();
+  const title = document.querySelector("#title").value;
+  const pages = document.querySelector("#pages").value;
+  const author = document.querySelector("#author").value;
+  const status = document.querySelector("#status").checked;
+  let newBook = new Book(title, author, pages, status);
+  myLibrary.push(newBook);
+  displayBook();
 }
 
-function addBookToLibrary(event){
-    event.preventDefault();
-    const title =document.querySelector("#title").value
-    const pages =document.querySelector("#pages").value
-    const author =document.querySelector("#author").value
-    const status =document.querySelector('#status').checked
-    let newBook = new Book (title, author, pages, status)
-    myLibrary.push(newBook);
-    displayBook();
-}
-
-function displayBook(){
-    const library =document.querySelector(".library");
-    library.innerHTML = '';
-    for (let i = 0; i < myLibrary.length; i++){
-        let book = myLibrary[i];
-        let card = document.createElement('div')
-        card.innerHTML = `<div class="card">
+function displayBook() {
+  const library = document.querySelector(".library");
+  library.innerHTML = "";
+  for (let i = 0; i < myLibrary.length; i++) {
+    let book = myLibrary[i];
+    let card = document.createElement("div");
+    card.innerHTML = `<div class="card">
         <h1 class="title">${book.title}</h1>
         <h2 class="author"> By: ${book.author}</h2>
         <h3 class="pages">Number of pages: ${book.pages}</h3>
-        <h4 class="status">${book.status ? 'Read' : 'Not read yet'}</h4>
+        <h4 class="status">${book.status ? "Read" : "Not read yet"}</h4>
         <button class="remove" onclick='removeBook(${i})'>Remove Book</button>
         <button class="toggle" onclick='readChange(${i})'>Change read status</button>
-      </div>`
-        library.appendChild(card)
-    }
+      </div>`;
+    library.appendChild(card);
+  }
 }
 
-
-function removeBook(index){
-    myLibrary.splice(index, 1);
-    displayBook();
+function removeBook(index) {
+  myLibrary.splice(index, 1);
+  displayBook();
 }
 
-const formDiv = document.querySelector('.form-cont');
-const bookForm = document.querySelector('.book-form');
-const addBtn = document.querySelector('.add-book')
-addBtn.addEventListener('click', function(){
-    if (formDiv.style.display = 'none'){
-        bookForm.style.display = 'flex';
-        formDiv.style.display = 'block'; 
-    }  
-    else if (bookForm.style.display = 'flex'){
-        bookForm.style.display = 'none';
-        formDiv.style.display = 'none'
-    }
-})
+const formDiv = document.querySelector(".form-cont");
+const bookForm = document.querySelector(".book-form");
+const addBtn = document.querySelector(".add-book");
+addBtn.addEventListener("click", function () {
+  if (formDiv.style.display == bookForm.style.display) {
+    formDiv.style.display = "block";
+    bookForm.style.display = "flex";
+  } else if (formDiv.style.display !== bookForm.style.display) {
+    formDiv.style.display = "none";
+    bookForm.style.display = "none";
+  }
+});
 
-const btnAdd = document.querySelector('.add');
-btnAdd.addEventListener('click', addBookToLibrary);
-
-
+const btnAdd = document.querySelector(".add");
+btnAdd.addEventListener("click", addBookToLibrary);
